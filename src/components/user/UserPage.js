c1103e649a8152c34154a7ef8fa234ec6fc4cdcf
KrissKry <edmaister02@gmail.com>
2021-06-10 00:26:16 +0200

    user panel redesign

--- src/components/user/UserPage.js
+++ src/components/user/UserPage.js
@@ -1,80 +1,56 @@
-import { Accordion, AccordionDetails, AccordionSummary, Button, Typography, withStyles } from '@material-ui/core';
-import React, { useState } from 'react';
+import {Button, Typography } from '@material-ui/core';
+import React from 'react';
 import { connect } from 'react-redux';
+import FormatListBulletedIcon from '@material-ui/icons/FormatListBulleted';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import KeyboardReturnIcon from '@material-ui/icons/KeyboardReturn';
+import SettingsIcon from '@material-ui/icons/Settings';
 
 import './UserPage.css';
-import Wishlist from './Wishlist';
-
-const CustomAccordion = withStyles( () => ({
-    root: {
-        marginTop: 10,
-        width: '75%',
-        // height: '15%',
-        background: 'rgba(0,0,0,0)',
-        textAlign: 'center',
-        alignContent: 'center',
-        alignItems: 'center',
-        borderColor: '#C51162',
-        color: '#C51162',
-        borderWidth: 1,
-        borderRadius: 5,
-        boxShadow: 'box-shadow: 10px 2px 10px 2px rgba(0,0,0,.2);',
-        // marginTop: '20px',
-        
-    },
-    expanded: {
-    //     // width: '100%',
-        height: '50%',
-    }
-}))(Accordion);
 
 
 
 const UserPage = props => {
 
 
-    const [expanded, setExpanded] = useState('');
 
-    const handleChange = (panel) => (event, newExpanded) => {
-        setExpanded(newExpanded ? panel : false);
-    }
 
-    const welcomeMsg = `Witaj >>>${props.user.name}<<<`;
+    const welcomeMsg = `Witaj,`;// >>>${props.user.name}<<<`;
     return (
         <div id='user-page' className='user-page'>
-            {/* <div id='user-panel' className='user-page-panel'>
 
-                <div id='user-panel-title-view' className='user-page-panel-title-view'>
-                    <Typography className='user-page-panel-title'>{welcomeMsg}</Typography>
+            <div id='user-page-navigator' className='user-page-navigator'>
+                <div className='user-page-welcome-message'>
+                    <Typography variant='body2'>{welcomeMsg}</Typography>
+                    <Typography variant='h6'> {props.user.name}</Typography>
                 </div>
 
-                <div id='user-panel-content' className='user-page-panel-content'>
-                    <CustomAccordion variant='outlined' expanded={expanded == 'wishlist'} onChange={handleChange('wishlist')}>
-                        <AccordionSummary className='user-page-panel-accordion-summary' >
-                            <Typography>Lista Życzeń</Typography>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            <Wishlist items={props.wishlist}/>
-                        </AccordionDetails>
-                    </CustomAccordion>
-                    <CustomAccordion variant='outlined' expanded={expanded == 'orders'} onChange={handleChange('orders')}>
-                        <AccordionSummary >
-                            <Typography>Zamówienia</Typography>
-                        </AccordionSummary>
-                    </CustomAccordion>
-                    <CustomAccordion variant='outlined'>
-                        <AccordionSummary >
-                            <Typography>Xd</Typography>
-                        </AccordionSummary>
-                    </CustomAccordion>  *
-                    
-                </div>
-                <div id='user-panel-end' className='user-page-panel-logout'>
+                <div className='user-page-navigator-buttons'>
+                    {/* <Typography variant='body2'>{welcomeMsg}</Typography> */}
                     <Button>
-                        <Typography>Wyloguj</Typography>
+                        <FormatListBulletedIcon />
+                        <Typography className='user-page-navigator-buttons-typo'>Zamówienia</Typography>
+                    </Button>
+                    <Button>
+                        <FavoriteBorderIcon />
+                        <Typography className='user-page-navigator-buttons-typo'>Lista życzeń</Typography>
+                    </Button>
+                    <Button>
+                        <KeyboardReturnIcon />
+                        <Typography className='user-page-navigator-buttons-typo'>Zwroty i reklamacje</Typography>
+                    </Button>
+                    
+                    <Button>
+                        <SettingsIcon />
+                        <Typography className='user-page-navigator-buttons-typo'>Ustawienia konta</Typography>
                     </Button>
                 </div>
-            </div> */}
+            </div>
+
+            <div id='user-page-body' className='user-page-body'>
+                
+            </div>
+            
         </div>
     )
 }
