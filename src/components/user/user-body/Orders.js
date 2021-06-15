798d5ed75bdc973469444e7a967f57684d42e248
KrissKry <edmaister02@gmail.com>
2021-06-15 14:58:15 +0200

    display shipping costs and discount

--- src/components/user/user-body/Orders.js
+++ src/components/user/user-body/Orders.js
@@ -8,10 +8,10 @@ import './Orders.css';
 
 const Orders = props => {
 
-    const RenderOrder = ({item, index}) => {
-        console.log('renderorder', item, index);
-        return (
+    const RenderOrder = ({item, index}) => (
         <Accordion key={item.id} style={{marginTop: 10, border: '1px solid #ccc'}}>
+
+            {/* order header */}
             <AccordionSummary className='user-order-accordion-summary'>
                 <Typography className='index'>{index + 1}</Typography>
                 <div className='id-div'>
@@ -21,18 +21,33 @@ const Orders = props => {
                 <Typography className='price'>{item.totalPrice}zł</Typography>
             </AccordionSummary>
 
+            {/* order details */}
             <AccordionDetails className='user-order-accordion-details'>
+                
+                {/* items ordered */}
                 {item.items.map( (item, index) => (
                     <div className='row'>
                         <Typography className='title'>{item.item.title}</Typography>
                         <Typography className='price-quantity'>{item.quantity}x{item.item.price} = {item.quantity*item.item.price}zł</Typography>   
                     </div>
                 ))}
+
+                {/* shipping and discount */}
+                <div className='bottom-row'>
+                    <Typography className='title'>Koszt dostawy</Typography>
+                    <Typography className='price-quantity'>{item.shippingPrice}zł</Typography>
+                </div>
+                <div className='bottom-row'>
+                    <Typography className='title'>Rabat</Typography>
+                    <Typography className='price-quantity'>{item.discount}zł</Typography>
+                </div>
+
             </AccordionDetails>
         </Accordion>
-    )}
+    )
     
-    console.log('zamowienia:', props);
+
+
     return (
         <div className='user-orders-container'>
             <Typography className='title'>Twoje Zamówienia</Typography>
